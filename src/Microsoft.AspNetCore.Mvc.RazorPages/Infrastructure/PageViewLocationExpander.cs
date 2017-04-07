51b98f70fdf8f6aebc4543a02054cdc68d8bfb7a
Ryan Nowak <nowakra@gmail.com>
2017-04-06 22:32:31 -0700

    pages view search prototype

---
+++ src/Microsoft.AspNetCore.Mvc.RazorPages/Infrastructure/PageViewLocationExpander.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
+{
+    public class PageViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            if (string.IsNullOrEmpty(context.PageName))
+            {
+                // Not a page - just act natural.
+                return viewLocations;
+            }
+
+            return ExpandPageHierarchy();
+
+            IEnumerable<string> ExpandPageHierarchy()
+            {
+                foreach (var location in viewLocations)
+                {
+                    if (!location.Contains("{1}/"))
+                    {
+                        // If the location doesn't have the 'page' replacement token just return it as-is.
+                        yield return location;
+                    }
+
+                    // For locations with the 'page' token - expand them into an ascending directory search,
+                    // but only up to the pages root.
+                    //
+                    // This is easy because the 'page' token already trims the root directory.
+                    var end = context.PageName.Length;
+
+                    while (end > 0 && (end = context.PageName.LastIndexOf('/', end - 1)) != -1)
+                    {
+                        // PageName always starts with `/`
+                        yield return location.Replace("{1}/", context.PageName.Substring(1, end));
+                    }
+                }
+            }
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            // The value we care about - 'page' is already part of the system. We don't need to add it manually.
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Mvc.RazorPages.Test/Infrastructure/PageViewLocationExpanderTest.cs
@@ -0,0 +1,144 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Razor.Language;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure
+{
+    public class PageViewLocationExpanderTest
+    {
+        [Fact]
+        public void PopulateValues_DoesNothing()
+        {
+            // Arrange
+            var context = CreateContext();
+
+            var expander = CreateViewLocationExpander();
+
+            // Act
+            expander.PopulateValues(context);
+
+            // Assert
+            Assert.Empty(context.Values);
+        }
+
+        [Fact]
+        public void ExpandLocations_NoOp_ForNonPage()
+        {
+            // Arrange
+            var context = CreateContext(pageName: null);
+            var locations = new string[]
+            {
+                "/ignore-me",
+            };
+
+            var expander = CreateViewLocationExpander();
+
+            // Act
+            var actual = expander.ExpandViewLocations(context, locations);
+
+            // Assert
+            Assert.Equal(locations, actual);
+        }
+
+        [Fact]
+        public void ExpandLocations_NoOp_WhenLocationDoesNotContainPageToken()
+        {
+            // Arrange
+            var context = CreateContext(pageName: null);
+            var locations = new string[]
+            {
+                "/ignore-me",
+            };
+
+            var expander = CreateViewLocationExpander();
+
+            // Act
+            var actual = expander.ExpandViewLocations(context, locations);
+
+            // Assert
+            Assert.Equal(locations, actual);
+        }
+
+        [Theory]
+        [InlineData("/Index", new string[] { "/{0}" })]
+        [InlineData("/Edit", new string[] { "/{0}" })]
+        [InlineData("/Customers/Add", new string[] { "/Customers/{0}", "/{0}" })]
+        public void ExpandLocations_ExpandsDirectories_WhenLocationContainsPage(
+            string pageName,
+            string[] expected)
+        {
+            // Arrange
+            var context = CreateContext(pageName: pageName);
+
+            var locations = new string[]
+            {
+                "/{1}/{0}",
+            };
+
+            var expander = CreateViewLocationExpander();
+
+            // Act
+            var actual = expander.ExpandViewLocations(context, locations);
+
+            // Assert
+            Assert.Equal(expected, actual.ToArray());
+        }
+
+        public void ExpandLocations_ExpandsDirectories_MultipleLocations()
+        {
+            // Arrange
+            var context = CreateContext(pageName: "/Pages/Customers/Edit");
+
+            var locations = new string[]
+            {
+                "/{1}/{0}",
+                "/More/Paths/{1}/{0}",
+                "/Views/Shared/{0}",
+            };
+
+            var expected = new string[]
+            {
+                "/Pages/Customers/{0}",
+                "/Pages/{0}",
+                "/More/Paths/Customers/{0}",
+                "/More/Paths/{0}",
+                "/Views/Shared/{0}",
+            };
+
+            var expander = CreateViewLocationExpander();
+
+            // Act
+            var actual = expander.ExpandViewLocations(context, locations);
+
+            // Assert
+            Assert.Equal(expected, actual.ToArray());
+        }
+
+        private ViewLocationExpanderContext CreateContext(string viewName = "_LoginPartial.cshtml", string pageName = null)
+        {
+            return new ViewLocationExpanderContext(
+                new ActionContext(),
+                viewName,
+                controllerName: null,
+                areaName: null,
+                pageName: pageName,
+                isMainPage: true)
+            {
+                Values = new Dictionary<string, string>(),
+            };
+        }
+
+        private PageViewLocationExpander CreateViewLocationExpander()
+        {
+            return new PageViewLocationExpander();
+        }
+    }
+}
